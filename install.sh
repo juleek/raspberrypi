ad63264bdffa395d11bccbd175ae193f5ab88391
dimanne <dimanne@ya.ru>
2014-12-11 23:27:51 +0300

    fix in install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,14 @@
 THIS_DIR=$(cd "$(dirname "$0")"; pwd)
 
 set -e
-set -x
 
 PROJECT_PATH="/home/pi/raspberrypi/"
 
 
 function InstallIfNeeded {
    local SERVICE="$1"
-   local INSTALLED="`md5sum /etc/systemd/system/$SERVICE`"
-   local NEW="`md5sum $THIS_DIR/$SERVICE`"
+   local INSTALLED="`md5sum /etc/systemd/system/$SERVICE | cut -d ' ' -f 1`"
+   local NEW="`md5sum $THIS_DIR/$SERVICE | cut -d ' ' -f 1`"
    
    echo "INSTALLED $INSTALLED"
    echo "NEW $NEW"
