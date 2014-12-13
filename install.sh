2c605bfaace9f31d1dbe9c7eda51679aeebcb29d
dimanne <dimanne@ya.ru>
2014-12-13 13:05:26 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-THIS_DIR=$(cd "$(dirname "$0")"; pwd)
-
 set -e
+
+THIS_DIR=$(cd "$(dirname "$0")"; pwd)
+cd "$THIS_DIR"
 
 PROJECT_PATH="/home/pi/raspberrypi/"
 
 
-function RunVerbosely() { echo "$@" ; "$@" ; }
+function RunVerbosely() { echo "update_thermo: $@" ; "$@" ; }
 
 function InstallIfNeeded {
    local SERVICE="$1"
+   local RESTART="$2"
    local INSTALLED="`md5sum /etc/systemd/system/$SERVICE | cut -d ' ' -f 1`"
    local NEW="`md5sum $THIS_DIR/$SERVICE | cut -d ' ' -f 1`"
    
@@ -21,11 +23,14 @@ function InstallIfNeeded {
       echo "$SERVICE not changed => skipping it"
    else 
       echo "$SERVICE changed => installing it"
-      RunVerbosely sudo systemctl stop $SERVICE
       RunVerbosely cp $SERVICE /etc/systemd/system/$SERVICE
-      RunVerbosely sudo systemctl daemon-reload
-      RunVerbosely sudo systemctl enable $SERVICE
-      RunVerbosely sudo systemctl start $SERVICE
+      RunVerbosely systemctl daemon-reload
+      RunVerbosely systemctl enable $SERVICE
+
+      if [ "$RESTART" = "true" ]
+      then
+         RunVerbosely systemctl restart $SERVICE
+      fi
    fi
 }
 
@@ -33,5 +38,6 @@ function InstallIfNeeded {
 cd $PROJECT_PATH
 git pull
 
-InstallIfNeeded reversessh.service
-InstallIfNeeded thermo.service
+InstallIfNeeded reversessh.service true
+InstallIfNeeded thermo.service true
+InstallIfNeeded update_thermo.service
