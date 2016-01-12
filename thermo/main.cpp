aa29103738aa91f7cff91bf14547b133cf675b91
dimanne <dimanne@ya.ru>
2016-01-12 08:35:16 +0300

    sdf

--- thermo/main.cpp
+++ thermo/main.cpp
@@ -6,9 +6,9 @@
 #include <QCommandLineParser>
 #include <QtDebug>
 
-void InProcTests(QString SmsPass) {
-   TSmsSender SmsSender("dimanne", SmsPass, "Tarasovka", { {0, {QTime(0, 0, 10)}} });
-   SmsSender.Send(0, "text of message", { {"+79647088442"} });
+//void InProcTests(QString SmsPass) {
+//   TSmsSender SmsSender("dimanne", SmsPass, "Tarasovka", { {0, {QTime(0, 0, 10)}} });
+//   SmsSender.Send(0, "text of message", { {"+79647088442"} });
    //   QThread::sleep(25);
    //   SmsSender.Send(0, "123", { {"+79647088442"} });
    //   QThread::sleep(25);
@@ -16,7 +16,7 @@ void InProcTests(QString SmsPass) {
    //   QThread::sleep(25);
    //   SmsSender.Send(0, "678", { {"+79647088442"} });
 
-}
+//}
 
 int main(int argc, char **argv) {
    QCoreApplication app(argc, argv);
@@ -29,13 +29,13 @@ int main(int argc, char **argv) {
    QString SMSPass = Parser.value(SMSPassOpt);
    qDebug() << "SMSPass:" << SMSPass;
 
-   InProcTests(SMSPass);
+   //InProcTests(SMSPass);
 
    const std::vector<TSensorInfo> SensorInfos = {
       { "/sys/bus/w1/devices/28-000005eac50a/w1_slave", "BottomTube", 12 },
       { "/sys/bus/w1/devices/28-000005eaddc2/w1_slave", "Ambient"   , 6  }
    };
-   const QTime SendSMSStartTime = QTime(18, 30, 0);
+   const QTime SendSMSStartTime = QTime(8, 30, 0);
    const QTime SendSMSEndTime   = QTime(19, 30, 0);
 
    new TDriver(SMSPass, SensorInfos, SendSMSStartTime, SendSMSEndTime);
