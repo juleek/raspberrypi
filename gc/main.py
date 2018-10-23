b7a2d8b45c65e213861026463713e52cae6524e5
DimanNe <dimanne@gmail.com>
2018-10-23 20:03:03 +0300

    big query

--- gc/main.py
+++ gc/main.py
@@ -5,40 +5,32 @@ import google.api_core.exceptions
 
 class GCloudState:
    def __init__(self, dataset_id, table_id, location):
-      self.dataset_id = dataset_id
-      self.table_id = table_id
-      self.location = location
-      self.client = bigquery.Client();
-      self.dataset_ref = self.client.dataset(self.dataset_id)
-      #self.table_ref = None
-      #self.table = None
-      self.do_init()
+      self.dataset_id  = dataset_id
+      self.table_id    = table_id
+      self.location    = location
+      self.client      = bigquery.Client();
 
-   # https://cloud.google.com/functions/docs/bestpractices/tips#functions-graceful-termination-python
-   # https://googleapis.github.io/google-cloud-python/latest/bigquery/usage/datasets.html
-   def do_init(self):
+      # https://cloud.google.com/functions/docs/bestpractices/tips#functions-graceful-termination-python
+      # https://googleapis.github.io/google-cloud-python/latest/bigquery/usage/datasets.html
+      self.dataset_ref = self.client.dataset(self.dataset_id)
       datasets = list(self.client.list_datasets())
       # print (datasets)
       has_required_dataset = bool(datasets and [True for dataset in datasets if dataset.dataset_id == self.dataset_id])
       # print (has_required_dataset);
       # print('Datasets in project "{}":'.format(project))
-      # for dataset in datasets:  # API request(s)
-      #     print('\t{}'.format(dataset.dataset_id))
       if not has_required_dataset:
          print('Project "{}" does not contain dataset "{}" => creating it'.format(self.client.project, self.dataset_id))
          create_dataset(self)
-
-
 
       schema = [
           bigquery.SchemaField('ContextEventId'     , 'STRING'   , mode='REQUIRED'),
           bigquery.SchemaField('Timestamp'          , 'TIMESTAMP', mode='REQUIRED'),
           bigquery.SchemaField(sensor_id_ambient    , 'FLOAT64'  , mode='NULLABLE'),
           bigquery.SchemaField(sensor_id_bottom_tube, 'FLOAT64'  , mode='NULLABLE'),
+          bigquery.SchemaField(error_string_id      , 'STRING'   , mode='NULLABLE'),
       ]
       self.table_ref = self.dataset_ref.table(self.table_id)
-      self.table = bigquery.Table(self.table_ref, schema=schema)
-
+      self.table     = bigquery.Table(self.table_ref, schema=schema)
       tables = list(self.client.list_tables(self.dataset_ref))  # API request(s)
       # print (tables)
       has_required_table = bool(tables and [True for table in tables if table.table_id == self.table_id])
@@ -48,12 +40,6 @@ class GCloudState:
          create_table(self)
 
 
-
-sensor_id_bottom_tube = "BottomTube";
-sensor_id_ambient     = "Ambient";
-gcloud_state_global_var = GCloudState(dataset_id = "MainDataSet",
-                                      table_id   = "AllTempSensors",
-                                      location   = 'europe-west2');
 def create_dataset(gcloud_state):
    dataset = bigquery.Dataset(gcloud_state.dataset_ref)
    dataset.location = location
@@ -65,6 +51,7 @@ def create_dataset(gcloud_state):
    except google.api_core.exceptions.Conflict as exc:
       print('{}: {}'.format(type(exc), exc))
 
+
 def create_table(gcloud_state):
    try:
       gcloud_state.table = gcloud_state.client.create_table(gcloud_state.table)  # API request
@@ -75,13 +62,25 @@ def create_table(gcloud_state):
 
 
 # https://cloud.google.com/bigquery/streaming-data-into-bigquery#bigquery-stream-data-python
-def insert_new_row(ambient_temperature, bottom_tube_temperature):
-   rows_to_insert = [(u'evid', u'2018-02-12 13:54', ambient_temperature, bottom_tube_temperature)]
-   # print ('Inserting: {}'.format(rows_to_insert))
-   errors = gcloud_state_global_var.client.insert_rows(gcloud_state_global_var.table, rows_to_insert)  # API request
+def insert_new_row(gcloud_state, ambient_temperature, bottom_tube_temperature, error_string):
+   rows_to_insert = [(u'evid2', u'2018-02-23 14:56', ambient_temperature, bottom_tube_temperature, error_string)]
+   print ('Inserting: {}'.format(rows_to_insert))
+   errors = gcloud_state.client.insert_rows(gcloud_state.table, rows_to_insert)  # API request
    print (errors)
    assert errors == []
 
+
+
+# -------------------------------------------------------------------------------------------------------------------
+
+
+
+sensor_id_bottom_tube   = "BottomTube";
+sensor_id_ambient       = "Ambient";
+error_string_id         = "ErrorString"
+gcloud_state_global_var = GCloudState(dataset_id = "MainDataSet",
+                                      table_id   = "AllTempSensors",
+                                      location   = 'europe-west2');
 
 
 def on_new_telemetry(data, context): # https://cloud.google.com/functions/docs/writing/background
@@ -101,13 +100,18 @@ def on_new_telemetry(data, context): # https://cloud.google.com/functions/docs/w
 
    all_sensors = [sensor_id_ambient, sensor_id_bottom_tube]
    if not any(True for sensor in all_sensors if sensor in json):
-      print('Failed to find neither of "{}" in JSON. Available keys are: {}'.format(all_sensors, json.keys())); # Log Error
+      print('Found neither of "{}" in JSON. Available keys are: "{}"'.format(all_sensors, json.keys())); # Log Error
       return;
 
    ambient_temperature     = json[sensor_id_ambient]     if sensor_id_ambient     in json else None
    bottom_tube_temperature = json[sensor_id_bottom_tube] if sensor_id_bottom_tube in json else None
-   print ("Checks are passed: bottom_tube_temperature={}, ambient_temperature={}".format(bottom_tube_temperature, ambient_temperature))
-   insert_new_row(ambient_temperature=ambient_temperature, bottom_tube_temperature=bottom_tube_temperature)
+   error_string            = json[error_string_id]       if error_string_id       in json else None
+   print ("Checks are passed: bottom_tube_temperature={}, ambient_temperature={}, ErrorString={}".
+         format(bottom_tube_temperature, ambient_temperature, error_string))
+   insert_new_row(gcloud_state            = gcloud_state_global_var,
+                  ambient_temperature     = ambient_temperature,
+                  bottom_tube_temperature = bottom_tube_temperature,
+                  error_string            = error_string)
 
 
 
