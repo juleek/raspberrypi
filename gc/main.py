77b70dd9e414bf32827be0a6dbd5195d2aff39aa
DimanNe <dimanne@gmail.com>
2018-10-23 23:18:28 +0300

    better python code

--- gc/main.py
+++ gc/main.py
@@ -12,40 +12,34 @@ class GCloudState:
 
       # https://cloud.google.com/functions/docs/bestpractices/tips#functions-graceful-termination-python
       # https://googleapis.github.io/google-cloud-python/latest/bigquery/usage/datasets.html
+
+      # datasets = list(self.client.list_datasets())
+      # has_required_dataset = bool(datasets and [True for dataset in datasets if dataset.dataset_id == self.dataset_id])
+
       self.dataset_ref = self.client.dataset(self.dataset_id)
-      datasets = list(self.client.list_datasets())
-      # print (datasets)
-      has_required_dataset = bool(datasets and [True for dataset in datasets if dataset.dataset_id == self.dataset_id])
-      # print (has_required_dataset);
-      # print('Datasets in project "{}":'.format(project))
-      if not has_required_dataset:
+      try:
+         dataset = self.client.get_dataset(self.dataset_ref)
+      except google.api_core.exceptions.NotFound:
          print('Project "{}" does not contain dataset "{}" => creating it'.format(self.client.project, self.dataset_id))
          create_dataset(self)
 
-      schema = [
-          bigquery.SchemaField('ContextEventId'     , 'STRING'   , mode='REQUIRED'),
-          bigquery.SchemaField('Timestamp'          , 'TIMESTAMP', mode='REQUIRED'),
-          bigquery.SchemaField(sensor_id_ambient    , 'FLOAT64'  , mode='NULLABLE'),
-          bigquery.SchemaField(sensor_id_bottom_tube, 'FLOAT64'  , mode='NULLABLE'),
-          bigquery.SchemaField(error_string_id      , 'STRING'   , mode='NULLABLE'),
-      ]
       self.table_ref = self.dataset_ref.table(self.table_id)
-      self.table     = bigquery.Table(self.table_ref, schema=schema)
-      tables = list(self.client.list_tables(self.dataset_ref))  # API request(s)
-      # print (tables)
-      has_required_table = bool(tables and [True for table in tables if table.table_id == self.table_id])
-      if not has_required_table:
+      try:
+         self.table = self.client.get_table(self.table_ref)
+      except google.api_core.exceptions.NotFound:
          print('Dataset "{}" in project "{}" does not contain table "{}" => creating it'.
                 format(self.dataset_id, self.client.project, self.table_id))
-         create_table(self)
+         self.table = create_table(self)
+
+      ensure_table_scheme(self)
 
 
 def create_dataset(gcloud_state):
    dataset = bigquery.Dataset(gcloud_state.dataset_ref)
-   dataset.location = location
+   dataset.location = gcloud_state.location
    try:
       dataset = gcloud_state.client.create_dataset(dataset)  # API request
-      print('Dataset "{}" created.'.format(dataset.dataset_id))
+      print('Dataset "{}" created.\n'.format(dataset.dataset_id))
    except google.api_core.exceptions.AlreadyExists as exc:
       print('{}: {}'.format(type(exc).__name__, exc))
    except google.api_core.exceptions.Conflict as exc:
@@ -54,11 +48,35 @@ def create_dataset(gcloud_state):
 
 def create_table(gcloud_state):
    try:
-      gcloud_state.table = gcloud_state.client.create_table(gcloud_state.table)  # API request
-      print("Table schema: {}".format(table.schema))
-      print("Table num of rows: {}".format(table.num_rows))
+      gcloud_state.client.create_table(bigquery.Table(gcloud_state.table_ref))  # API request
+      print('Table "{}" created.\n'.format(gcloud_state.table_ref))
    except google.api_core.exceptions.Conflict as exc:
       print('{}: {}'.format(type(exc), exc))
+
+   table = gcloud_state.client.get_table(gcloud_state.table_ref)
+   return table
+
+
+
+def ensure_table_scheme(gcloud_state):
+   schema = [
+       bigquery.SchemaField('ContextEventId'     , 'STRING'   , mode='REQUIRED'),
+       bigquery.SchemaField('Timestamp'          , 'TIMESTAMP', mode='REQUIRED'),
+       bigquery.SchemaField(sensor_id_ambient    , 'FLOAT64'  , mode='NULLABLE'),
+       bigquery.SchemaField(sensor_id_bottom_tube, 'FLOAT64'  , mode='NULLABLE'),
+       bigquery.SchemaField(error_string_id      , 'STRING'   , mode='NULLABLE'),
+   ]
+   original_schema = gcloud_state.table.schema
+   # print ('Original schema: {}'.format(original_schema))
+   new_schema = original_schema[:]  # creates a copy of the schema
+   for schema_field in schema:
+      if not [True for existing_field in original_schema if existing_field.name == schema_field.name]:
+         print ('{} is not in original schema of table "{}" => adding it'.format(schema_field, gcloud_state.table.table_id))
+         new_schema.append(schema_field)
+
+   gcloud_state.table.schema = new_schema
+   gcloud_state.table = gcloud_state.client.update_table(gcloud_state.table, ['schema'])  # API request
+
 
 
 # https://cloud.google.com/bigquery/streaming-data-into-bigquery#bigquery-stream-data-python
@@ -80,7 +98,7 @@ sensor_id_ambient       = "Ambient";
 error_string_id         = "ErrorString"
 gcloud_state_global_var = GCloudState(dataset_id = "MainDataSet",
                                       table_id   = "AllTempSensors",
-                                      location   = 'europe-west2');
+                                      location   = "europe-west2")
 
 
 def on_new_telemetry(data, context): # https://cloud.google.com/functions/docs/writing/background
