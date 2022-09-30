e55485553de3bbbbeb27dd099df4868b92c18ecf
Yulia <jalbova@gmail.com>
2022-09-30 17:15:01 +0100

    topic

---
+++ google_functions_v2/topic.py
@@ -0,0 +1,19 @@
+from google.cloud import pubsub_v1
+from google.api_core import exceptions
+
+
+def create_topic_if_not_exists(client: pubsub_v1.PublisherClient, topic_path: str):
+    try:
+        client.get_topic(topic=topic_path)
+    except exceptions.NotFound:
+        client.create_topic(request={"name": topic_path})
+
+
+def create_topic_and_publish_msg(project_id: str, topic_id: str, msg: str):
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
+    create_topic_if_not_exists(publisher, topic_path)
+
+    future = publisher.publish(topic_path, msg.encode())
+    message_id = future.result()
+    print(f'message_id = {message_id}')
