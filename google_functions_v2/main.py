d75fcf123846f38052c865c6198b67cfd91293f9
Yulia <jalbova@gmail.com>
2022-10-08 17:53:48 +0100

    address comments

--- google_functions_v2/main.py
+++ google_functions_v2/main.py
@@ -23,9 +23,12 @@ TOPIC_ID: str = "tarasovka_topic"
 @functions_framework.cloud_event
 def google_ingest(cloud_event):
     chat_id_db: chidb.ChatIdDB = chidb.ChatIdDB(project=PROJECT, dataset_id=DATASET_ID, location=LOCATION)
-    consumer_db: sdb.Consumer = sdb.Consumer(sdbq.SensorsDBBQ(project=PROJECT, dataset_id=DATASET_ID, location=LOCATION))
-    consumer_alert: alt.Alerting = alt.Alerting(name_min_tuples=[(botnotif.AMBIENT_TUBE, botnotif.ambient_alert_temperature),
-                                                                 (botnotif.BOTTOM_TUBE, botnotif.bottom_tube_alert_temperature)],
+    consumer_db: sdb.Consumer = sdb.Consumer(sdbq.SensorsDBBQ(project=PROJECT,
+                                                              dataset_id=DATASET_ID,
+                                                              location=LOCATION,
+                                                              table_name="sensors_db"))
+    consumer_alert: alt.Alerting = alt.Alerting(name_min_tuples=[{botnotif.AMBIENT_TUBE: botnotif.ambient_alert_temperature,
+                                                                  botnotif.BOTTOM_TUBE: botnotif.bottom_tube_alert_temperature}],
                                                 sender=tel_s.TelegramSender(chat_id_db.read(alt.NAME), alt.ID))
 
 
@@ -58,12 +61,13 @@ def on_alerting_bot_message(request: flask.Request):
     chat_id: int = tel_s.get_chat_id_from_update_msg(request.data.decode("utf-8"))
     db: chidb.ChatIdDB = chidb.ChatIdDB(project=PROJECT, dataset_id=DATASET_ID, location=LOCATION)
     db.ask_to_add(chat_id, tel_s.TelegramSender(chat_id, alt.ID), alt.NAME)
+
     return 'OK'
 
 
 
 @functions_framework.http
-def scheduler(request: flask.Request):
+def on_cron(request: flask.Request):
     botnotif.notify(sdbq.SensorsDBBQ(project=PROJECT, dataset_id=DATASET_ID, location=LOCATION),
                     chidb.ChatIdDB(project=PROJECT, dataset_id=DATASET_ID, location=LOCATION))
 
