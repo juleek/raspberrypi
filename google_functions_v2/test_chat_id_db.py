0125d9feb634176ef816a6f6c379dea75c3db92b
Yulia <jalbova@gmail.com>
2022-10-09 13:55:44 +0100

    Fix tests

--- google_functions_v2/test_chat_id_db.py
+++ google_functions_v2/test_chat_id_db.py
@@ -7,12 +7,12 @@ class TestChatIdDB(unittest.TestCase):
         self.db: chiddb.ChatIdDB = chiddb.ChatIdDB(project="tarasovka",
                                                    dataset_id="test",
                                                    location="europe-west2")
-        self.chat_id: int = 123456
+        self.chat_id: int = 12345656565
         self.bot_name: str = "Notifier_bot"
 
     def test_ask_to_add(self):
         self.db.ask_to_add(self.chat_id, stdout.StdoutSender(), self.bot_name)
 
     def test_read_chat_id_from_db(self):
-        result = self.db.read_chat_id_from_db(self.bot_name)
+        result = self.db.read(self.bot_name)
         self.assertEqual(result, self.chat_id)
