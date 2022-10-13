6028615b09ea8754d1868125d88e3bdd016dc257
Yulia <jalbova@gmail.com>
2022-10-13 15:52:30 +0100

    added markdown in function send_text

--- google_functions_v2/stdout_sender.py
+++ google_functions_v2/stdout_sender.py
@@ -2,8 +2,8 @@
 import sender
 
 
-class StdoutSender(s.Sender):
-    def send_text(self, text: str) -> sender.SendResult:
+class StdoutSender(sender.Sender):
+    def send_text(self, text: str, is_markdown: bool) -> sender.SendResult:
         print(text)
         return sender.SendResult(is_ok=True, http_code=200)
 
