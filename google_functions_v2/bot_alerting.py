b1ae8eec106812ed42de458dc5ca755fc578d043
Yulia <stk.yulia@gmail.com>
2022-10-14 10:48:19 +0100

    bot_alerting (#29)

--- google_functions_v2/bot_alerting.py
+++ google_functions_v2/bot_alerting.py
@@ -3,15 +3,19 @@ import devicedatum as dd
 import ingest
 import sender as s
 import typing as t
+import secrets_bot as sec_bot
+
+BOT_NAME: str = "Alerting_bot"
+BOT_ID: str = sec_bot.alerting_bot_id
 
 class Alerting(ingest.Consumer):
-    def __init__(self, name_min_tuples: t.List[t.Tuple[str, float]], sender: s.Sender):
-        self.name_min_tuples = name_min_tuples
+    def __init__(self, name_to_min: t.Dict[str, float], sender: s.Sender):
+        self.name_to_min = name_to_min
         self.sender = sender
 
     def consume(self, datum: dd.DeviceDatum) -> None:
         messages: t.List[str] = []
-        for name, min_temp in self.name_min_tuples:
+        for name, min_temp in self.name_to_min:
             if name not in datum.name_to_temp:
                 continue
             if datum.name_to_temp[name] > min_temp:
