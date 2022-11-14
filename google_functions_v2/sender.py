bf0093e25f6f41628c10190df3d00ce8ab18b05a
Yulia <stk.yulia@gmail.com>
2022-11-14 11:33:35 +0000

    Sender_description (#35)

--- google_functions_v2/sender.py
+++ google_functions_v2/sender.py
@@ -5,10 +5,14 @@ from dataclasses import dataclass
 
 @dataclass
 class SendResult:
+    """Represents status of sending a notification via Sender."""
     is_ok: bool
     http_code: int
 
 class Sender(abc.ABC):
+    """Abstract class that responsible for sending text and pic to user.
+    This class is helping to implement different types of communication apps that allows us to send notifications, such as email, messenger, sms and etc."""
+
     @abc.abstractmethod
     def send_with_pic(self, text: str, pic) -> SendResult:
         pass
