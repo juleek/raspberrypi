e48d440a592d5c37ef997f4ae1990bba33447df1
Yulia <jalbova@gmail.com>
2022-11-28 13:41:23 +0000

    address comments

--- google_functions_v2/test_alerting.py
+++ google_functions_v2/test_alerting.py
@@ -11,19 +11,19 @@ class TestAlerting(unittest.TestCase):
     def setUp(self):
         self.name_to_min = {"Ambient": 6, "BottomTube": 12}
         self.sender = mc.Mock(spec=sender.Sender)
-        self.alert_obj = botalert.Alerting(self.name_to_min, self.sender)
+        self.alerting = botalert.Alerting(self.name_to_min, self.sender)
         self.tube_1 = "Ambient"
         self.tube_2 = "BottomTube"
-        self.datum_with_min_temp: dd.DeviceDatum = dd.DeviceDatum({self.tube_1: 4, self.tube_2: 11}, dt.datetime(2011, 11, 4, 0, 0, tzinfo=pytz.UTC), "")
-        self.datum_with_no_min_temp: dd.DeviceDatum = dd.DeviceDatum({self.tube_1: 7, self.tube_2: 13}, dt.datetime(2011, 11, 4, 0, 0, tzinfo=pytz.UTC), "")
+        self.datum_below_threshold: dd.DeviceDatum = dd.DeviceDatum({self.tube_1: 4, self.tube_2: 11}, dt.datetime(2011, 11, 4, 0, 0, tzinfo=pytz.UTC), "")
+        self.datum_above_threshold: dd.DeviceDatum = dd.DeviceDatum({self.tube_1: 7, self.tube_2: 13}, dt.datetime(2011, 11, 4, 0, 0, tzinfo=pytz.UTC), "")
 
 
     def test_alerting_sends_message_if_temp_below_threshold(self):
-        self.alert_obj.consume(self.datum_with_min_temp)
+        self.alerting.consume(self.datum_below_threshold)
         self.sender.send_text.assert_called()
         self.assertTrue(self.sender.send_text.call_args[0][0])
 
 
     def test_alerting_does_not_send_message_if_temp_above_threshold(self):
-        self.alert_obj.consume(self.datum_with_no_min_temp)
+        self.alerting.consume(self.datum_above_threshold)
         self.sender.send_text.assert_not_called()
