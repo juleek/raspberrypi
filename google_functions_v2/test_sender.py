b915d5ce15a31840ce479c0257a102cb2372cf13
Yulia <jalbova@gmail.com>
2022-12-11 17:56:36 +0000

    fixed test

--- google_functions_v2/test_sender.py
+++ google_functions_v2/test_sender.py
@@ -5,4 +5,4 @@ import telegram_sender as ts
 class TestTelegramSender(unittest.TestCase):
     def test_get_chat_id_get_chatid_from_str(self):
         result = ts.get_chatid_from_str('{"update_id":802398224, "message":{"message_id":23,"from":{"id":1111,"first_name":"Y","last_name":"Y", "username":"y","language_code":"en"},"chat":{"id":111,"title":"title","type": "group"},"date":1660129735}}')
-        self.assertEqual(result, -748244195)
+        self.assertEqual(result, 111)
