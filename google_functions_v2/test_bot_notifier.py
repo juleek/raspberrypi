bf0ea3583c37cb6a952a55085608872800703607
Yulia <stk.yulia@gmail.com>
2022-12-11 17:45:23 +0000

    Apply suggestions from code review
    Co-authored-by: DimanNe <causal.consistency@gmail.com>

--- google_functions_v2/test_bot_notifier.py
+++ google_functions_v2/test_bot_notifier.py
@@ -85,7 +85,7 @@ class TestCreateMsgWithCurrTemp(unittest.TestCase):
         actual: str = botnotifier.create_msg_with_current_temp([], self.with_error_msgs)
         check_error_part(self, actual)
 
-    def test_create_msg_with_sensors_and_empty_error_msg_return_msg_with_sensors(self):
+    def test_sensors_and_empty_error_msg_return_msg_with_sensors(self):
         actual: str = botnotifier.create_msg_with_current_temp(self.sensors, set())
         check_sensor_part(self, actual)
 
