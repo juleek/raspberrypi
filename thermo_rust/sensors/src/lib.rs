78d0c1c46f5689df37799687bed742fa3cd54ff2
dimanne <dimanne@gmail.com>
2023-01-01 16:26:40 +0000

    Rust thermo: Add Sensor trait

--- thermo_rust/sensors/src/lib.rs
+++ thermo_rust/sensors/src/lib.rs
@@ -1,32 +1,21 @@
 #[allow(non_snake_case)]
 mod DS18B20;
 
-// #[derive(PartialEq, Debug)]
-// struct ErrorDesc(String);
-// type Result<T> = std::result::Result<T, ErrorDesc>;
-// impl ErrorDesc {
-//    pub fn new_err<T>(function_name: &str, desc: &str) -> Result<T> {
-//       Err(ErrorDesc(format!("Failed to {function_name}: {desc}")))
-//    }
-// }
-
-// impl<T: std::fmt::Debug> From<T> for ErrorDesc {
-//    fn from(value: T) -> Self {
-//       // ErrorDesc(TODO: print value in a string...);
-//     }
-// }
-
-pub fn add(left: usize, right: usize) -> usize {
-   left + right
+trait Sensor {
+   fn new(id: i32, path: &str) -> Self;
+   fn id(&self) -> i32;
+   fn path(&self) -> &str;
+   fn read(&self) -> f64;
 }
 
 #[cfg(test)]
 mod tests {
+   #[allow(unused_imports)]
    use super::*;
 
    #[test]
    fn it_works() {
-      let result = add(2, 2);
+      let result = 2 + 2;
       assert_eq!(result, 4);
    }
 }
