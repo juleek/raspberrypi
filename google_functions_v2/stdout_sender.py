227d0b0bb13fb3dc743ca77fe833c459cfa0fc15
Yulia <stk.yulia@gmail.com>
2023-05-15 22:21:47 +0100

    Delete excessive prints

--- google_functions_v2/stdout_sender.py
+++ google_functions_v2/stdout_sender.py
@@ -4,9 +4,9 @@ import sender
 
 class StdoutSender(sender.Sender):
     def send_text(self, text: str, is_markdown: bool) -> sender.SendResult:
-        print(text)
+        # print(text)
         return sender.SendResult(is_ok=True, http_code=200, err_str="")
 
     def send_with_pic(self, text: str, pic) -> sender.SendResult:
-        print(text)
+        # print(text)
         return sender.SendResult(is_ok=True, http_code=200, err_str="")
