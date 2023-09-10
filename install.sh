df35e1b96252690c860ed7898d6832ae82592267
dimanne <causal.consistency@proton.me>
2023-09-10 20:29:44 +0100

    swiss tunnel

--- install.sh
+++ install.sh
@@ -57,6 +57,7 @@ else
    echo "thermo has been built and restarted"
 fi
 
+
 # Too critical to be updated on regular basis
 # InstallIfNeeded /etc/systemd/system reversessh.service true
 InstallIfNeeded /etc/systemd/system reversessh-swiss.service true
