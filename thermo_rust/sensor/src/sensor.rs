009dce2bc8bd4af9dcd72be6716dc1699f29be52
Yulia <stk.yulia@gmail.com>
2024-05-28 23:31:48 +0100

    Add descriptions of test cases

--- thermo_rust/sensor/src/sensor.rs
+++ thermo_rust/sensor/src/sensor.rs
@@ -1,24 +1,84 @@
 use anyhow::{anyhow, Context, Result};
 use std::io::Read;
 
-fn get_temp() -> Result<(f64, f64)> {
-   let mut res = (0.0, 0.0);
-   let sens1 = std::path::PathBuf::from("/sys/bus/w1/devices/28-000005eaddc2/w1_slave");
-   let sens2 = std::path::PathBuf::from("/sys/bus/w1/devices/28-000005eac50a/w1_slave");
+fn parse(sensor_info: &str) -> Result<f64> {
+   let lines: Vec<&str> = sensor_info.lines().collect();
+   if lines.is_empty() || lines.len() == 1 {
+      return Err(anyhow!("Failed to parse lines. Lines: {lines:?}"));
+   }
+   let start_index = lines[1].find("t=");
+   if start_index == None {
+      return Err(anyhow!("Failed to find start_index in line {:?}", lines[1]));
+   }
+   let temp_str = &lines[1][start_index + 2..];
+   let temp_float: Result<f64> = temp_str.parse();
+   assert!(temp_float.is_ok());
+   let temp = temp_float.unwrap() / 1000.0;
+   Ok(temp)
+}
 
-   for path in [sens1, sens2] {
-      let mut read_file = std::fs::File::open(path)?;
-      let mut contents = String::new();
-      read_file.read_to_string(&mut contents);
-      let lines: Vec<&str> = contents.lines().collect();
-      let start_index = lines[1].find("t=").unwrap();
-      let temp = &lines[1][start_index + 2..];
-      if res.0 == 0.0 {
-         res.0 = temp.parse().unwrap();
-      } else {
-         res.1 = temp.parse().unwrap();
+
+#[cfg(test)]
+mod tests {
+   use super::*;
+   use pretty_assertions::{assert_eq, assert_ne, assert_str_eq};
+
+   #[derive(Debug)]
+   struct OkParseTC {
+      data:   String,
+      expected: f64,
+   }
+
+   #[test]
+   fn test_parse_returns_ok() {
+      let test_cases = [
+         //
+         OkParseTC { data:   String::from("26: crc=64 YES\n 26 t=18375"), expected: 18.375, },
+         OkParseTC { data:   String::from("26: crc=64 YES\n 26 t=375"), expected: 0.375, },
+
+
+         // normal
+         // leading zeros 0100 => 0.1
+         // negative
+         // more than 2 lines, 2nd line has correct data
+
+         //
+         ];
+
+      for (i, tc) in test_cases.iter().enumerate() {
+         let res = parse(&tc.data);
+         assert!(res.is_ok());
+         assert_eq!(res.unwrap(), tc.expected, "Test-case #{i}: {tc:?}");
       }
    }
-   Ok(res)
 
+   struct ErrorParseTC {
+      data: String,
+   }
+
+   #[test]
+   fn test_parse_returns_error() {
+      let test_cases = [
+         //
+         ErrorParseTC { data: String::new() },
+         ErrorParseTC { data: String::from("26: crc=64 YES"), },
+         //
+
+
+         // empty
+         // one line
+         // t= in 1st line only
+         // letters after number
+         // letters before number
+         // multiple t=
+         ];
+
+      for tc in test_cases {
+         let res = parse(&tc.data);
+         println!("{res:?}");
+         assert!(res.is_err());
+      }
+   }
 }
+
+// println!()
