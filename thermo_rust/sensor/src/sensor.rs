487491d49147a4560a92087812e5aba8f4a7271b
Yulia <stk.yulia@gmail.com>
2024-05-29 21:52:37 +0100

    Parse

--- thermo_rust/sensor/src/sensor.rs
+++ thermo_rust/sensor/src/sensor.rs
@@ -1,20 +1,24 @@
 use anyhow::{anyhow, Context, Result};
 use std::io::Read;
 
-fn parse(sensor_info: &str) -> Result<f64> {
-   let lines: Vec<&str> = sensor_info.lines().collect();
-   if lines.is_empty() || lines.len() == 1 {
-      return Err(anyhow!("Failed to parse lines. Lines: {lines:?}"));
-   }
-   let start_index = lines[1].find("t=");
-   if start_index == None {
-      return Err(anyhow!("Failed to find start_index in line {:?}", lines[1]));
-   }
-   let temp_str = &lines[1][start_index + 2..];
-   let temp_float: Result<f64> = temp_str.parse();
-   assert!(temp_float.is_ok());
-   let temp = temp_float.unwrap() / 1000.0;
-   Ok(temp)
+fn parse(data: &str) -> Result<f64> {
+   const MAX: usize = 2 * 1024;
+   let second_line = {
+      let data = &data[..std::cmp::min(MAX, data.len())];
+      let start = data.find('\n')
+                      .with_context(|| anyhow!("Failed to find start of second line in: {}", &data))?;
+      let data = &data[start + 1..];
+      let end = data.find('\n').unwrap_or(data.len());
+      &data[..end]
+   };
+   let temperature = {
+      let start = &second_line.find("t=").with_context(|| anyhow!("Failed to find t= in: {second_line}"))?;
+      let temp = &second_line[start + 2..];
+      let temp: i32 =
+         temp.parse().with_context(|| anyhow!("Failed to parse temperature as integer: {temp}"))?;
+      temp as f64 / 1000.0
+   };
+   Ok(temperature)
 }
 
 
@@ -25,7 +29,7 @@ mod tests {
 
    #[derive(Debug)]
    struct OkParseTC {
-      data:   String,
+      data:     String,
       expected: f64,
    }
 
@@ -33,17 +37,12 @@ mod tests {
    fn test_parse_returns_ok() {
       let test_cases = [
          //
-         OkParseTC { data:   String::from("26: crc=64 YES\n 26 t=18375"), expected: 18.375, },
-         OkParseTC { data:   String::from("26: crc=64 YES\n 26 t=375"), expected: 0.375, },
-
-
-         // normal
-         // leading zeros 0100 => 0.1
-         // negative
-         // more than 2 lines, 2nd line has correct data
-
+         OkParseTC { data:   String::from("26: crc=64 YES\n 26 t=18375"), expected: 18.375, },      // normal
+         OkParseTC { data:   String::from("26: crc=64 YES\n 26 t=0375"), expected: 0.375, },        // leading zeros 0100 => 0.1
+         OkParseTC { data:   String::from("26: crc=64 YES\n 26 t=-18375"), expected: -18.375, },    // negative
+         OkParseTC { data:   String::from("26: crc=64 YES\n 26 t=18375\n 26"), expected: 18.375, }, // more than 2 lines, 2nd line has correct data
          //
-         ];
+      ];
 
       for (i, tc) in test_cases.iter().enumerate() {
          let res = parse(&tc.data);
@@ -60,18 +59,14 @@ mod tests {
    fn test_parse_returns_error() {
       let test_cases = [
          //
-         ErrorParseTC { data: String::new() },
-         ErrorParseTC { data: String::from("26: crc=64 YES"), },
+         ErrorParseTC { data: String::new() },                                 // empty
+         ErrorParseTC { data: String::from("26: crc=64 YES"), },               // one line
+         ErrorParseTC { data: String::from("26: crc=64 YES t=1\n18325"), },    // t= in 1st line only
+         ErrorParseTC { data: String::from("26: crc=64 YES\n 26 t=375ABC"), }, // letters after number
+         ErrorParseTC { data: String::from("26: crc=64 YES\n 26 t=ABC375"), }, // letters before number
+         ErrorParseTC { data: String::from("26: crc=64 YES\n 26 t=1 t=2"), },  // multiple t=
          //
-
-
-         // empty
-         // one line
-         // t= in 1st line only
-         // letters after number
-         // letters before number
-         // multiple t=
-         ];
+      ];
 
       for tc in test_cases {
          let res = parse(&tc.data);
